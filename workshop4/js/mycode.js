775e4ec9d1661e23fc9fd5845dfe564b94f66108
Julia Kordick <52427852+MsJKrdck@users.noreply.github.com>
2021-09-21 12:45:36 +0200

    init

---
+++ workshop4/js/mycode.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+var scroll = "uninitialized";
+$("#navbar").hide();
+
+
+$(window).scroll(function(){
+
+     scroll = $(window).scrollTop();
+     
+     if (scroll > $("#section2").offset().top){
+         $("#navbar").show();
+     }
+     
+     if (scroll < $("#section2").offset().top){
+         $("#navbar").hide();
+     }     
+});
+});
